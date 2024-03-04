df82bdefb432b0329a8800dd4c7cc760ab606ffe
kafyle <schnurzelpopel@gmail.com>
2024-03-04 12:14:13 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Testing Tree",
+	id: "testingtree1234",
+	author: "Foxune",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
